a9b1f49fbf360017704b3b971d7ef3a59ce1c545
anna0625 <anna860625@gmail.com>
2022-10-04 17:20:27 +1100

    adding local storage

--- booklist/src/contexts/BookContext.jsx
+++ booklist/src/contexts/BookContext.jsx
@@ -1,13 +1,17 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, [
-    { title: "think like a monk", author: "jay shetty", id: 1 },
-    { title: "name of the wind", author: "patrick rothfuss", id: 2 },
-  ]);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   // const [books, setBooks] = useState([
   //   { title: "think like a monk", author: "jay shetty", id: 1 },
